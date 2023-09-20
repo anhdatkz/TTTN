d3cf3a441ed5becc9d48461b27bf3590c3f8ee6f
Your Name <you@example.com>
2023-09-20 16:43:11 +0700

    update

--- FE_USER/fe_user/src/api/apiConfigs.js
+++
@@ -1,6 +0,0 @@
-const apiConfigs = {
-    baseUrl: 'http://localhost:8081/api/v1'
-}
-
-
-export default apiConfigs;
--- FE_USER/fe_user/src/stores/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore, createReducer } from "@reduxjs/toolkit";
-import cartReducer from "../features/cartSlice";
-
-const store =  configureStore({
-    reducer:{
-        cart: cartReducer,
-    }
-})
-export default store;
